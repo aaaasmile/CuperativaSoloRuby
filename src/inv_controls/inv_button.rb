0c0dd2e66ec6e80f9a537d57f1a31860f7dde356
aaaasmile <testhaus@gmx.at>
2015-12-22 17:47:47 +0100

    Button display the label

--- src/inv_controls/inv_button.rb
+++ src/inv_controls/inv_button.rb
@@ -1,58 +1,51 @@
 # file: inv_button.rb
 
 require 'inv_widget'
+require 'inv_label'
 
 class InvButton < InvWidget
-  attr_accessor :caption, :font 
   
   def initialize(x=0, y=0, w=0, h=0, zord=0, visb=true, rot=false)
     super(x,y,w,h,zord,visb,rot)
-    @caption = ""
+    
     @min_width = 70
-    @min_height = 10
+    @min_height = 30
     check_min_height
     check_min_width
   end
   
   def set_content(cont)
     if cont.kind_of?(String)
-      @caption = cont
+      @content = InvLabel.new
+      @content.set_text(cont, :medium)
     end
   end
-  
-  def set_style(style)
-    if style == :auto
-      calculate_width
-      calculate_height
-    end
-  end
-  
+    
   def draw(dc, theme)
-    @font = theme.fonts_text[:medium]
-    @text_col = theme.fore_color
-    width_text = @font.getTextWidth(@caption)
-    dc.font = @font
-    dc.foreground = @text_col
-    dc.drawText(@pos_x + (@width - width_text) / 2, @pos_y + @height - 5, @caption)
+    return if @content == nil
+    @border_thik = 1
+    width_content = @content.calculate_width(theme)
+    height_content = @content.calculate_height(theme)
+    dc.foreground = theme.border_color
+    dc.drawRectangle(@pos_x, @pos_y, @width, @height)
+    
+    dc.foreground = theme.main_color
+    dc.fillRectangle(@pos_x + @border_thik, @pos_y + @border_thik, 
+                     @width - @border_thik * 2, 
+                     @height - @border_thik * 2)
+    
+    @content.pos_x = @pos_x + (@width - width_content) / 2
+    @content.pos_y = @pos_y + (@height - height_content) / 2
+    @content.draw(dc, theme)
   end
   
 private
-  def calculate_width
-    @width = @font.getTextWidth(@caption) + 10
-    check_min_width
+  def check_min_height
+    @height = @min_height if @min_height < @min_height
   end
   
   def check_min_width
     @width = @min_width if @width < @min_width
-  end
-  
-  def calculate_height
-    @height = @font.getTextHeight(@caption) + 10
-    check_min_height
-  end
-  
-  def check_min_height
-    @height = @min_height if @min_height < @min_height
   end
   
 end
---
+++ src/inv_controls/inv_label.rb
@@ -0,0 +1,38 @@
+# file: inv_label.rb
+
+
+class InvLabel < InvWidget
+  
+  def initialize(x=0, y=0, w=0, h=0, zord=0, visb=true, rot=false)
+    super(x,y,w,h,zord,visb,rot)
+    @caption = ""
+  end
+  
+  def set_text(text, size)
+    @caption = text
+    @font_size_class = size
+  end
+  
+  def calculate_width(theme)
+    @font = theme.fonts_text[@font_size_class]
+    @width = @font.getTextWidth(@caption)
+    return @width
+  end
+  
+  def calculate_height(theme)
+    @font = theme.fonts_text[@font_size_class]
+    @height = @font.getTextHeight(@caption)
+    return @height
+  end
+  
+  def draw(dc, theme)
+    @font = theme.fonts_text[@font_size_class]
+    @text_col = theme.fore_color
+    @pos_y = @pos_y + @height * 3 / 4 # draw text start near to the bottom
+    dc.font = @font
+    dc.foreground = @text_col
+    dc.drawText(@pos_x, @pos_y, @caption)
+  end
+  
+end
+
