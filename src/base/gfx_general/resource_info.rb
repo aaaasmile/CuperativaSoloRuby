53bc3cc513393d70db31b23ebe007d56573176d6
aaaasmile <testhaus@gmx.at>
2015-12-29 19:58:44 +0100

    Fix startup for win xp

--- src/base/gfx_general/resource_info.rb
+++ src/base/gfx_general/resource_info.rb
@@ -8,8 +8,13 @@ class ResourceInfo
 
   def self.get_dir_appdata
     res = ""
-    if $g_os_type != :linux
-      res = File.join(ENV['LOCALAPPDATA'], "Invido_it/CupUserData")
+    if $g_os_type == :win32_system
+      path_data = ENV['LOCALAPPDATA'] ? ENV['LOCALAPPDATA'] : ENV['APPDATA'] # Note: in XP LOCALAPPDATA does not exist.
+      res = File.join(path_data, "Invido_it")
+      if !File.directory?(res)
+        Dir.mkdir(res)
+      end
+      res = File.join(res, "CupUserData")
     else
       res = File.expand_path("~/.cuperativa")
       puts "We are on linux, data dir #{res}"
@@ -17,6 +22,7 @@ class ResourceInfo
     if !File.directory?(res)
       Dir.mkdir(res)
     end
+    puts "Dir app data is: #{res}"
     return res
   end
 
