85bceda3cdcae25941b926b44ea5186bf95d76bd
aaaasmile <testhaus@gmx.at>
2016-01-07 19:37:37 +0100

    removed duplicated stuff

--- test/mariazza/test_mariazza.rb
+++ test/mariazza/test_mariazza.rb
@@ -9,11 +9,14 @@ require 'log4r'
 require 'yaml'
 
 
-PATH_TO_CLIENT = File.expand_path(File.dirname(__FILE__) + '/../../src')
+$:.unshift File.dirname(__FILE__) + '/../../src'
+$:.unshift File.dirname(__FILE__) + '/../..'
 
-require File.join( PATH_TO_CLIENT, 'core/core_game_base')
-require File.join( PATH_TO_CLIENT, 'games/mariazza/core_game_mariazza')
-require File.join( PATH_TO_CLIENT, 'games/mariazza/alg_cpu_mariazza')
+require 'core/core_game_base'
+require 'games/mariazza/core_game_mariazza'
+require 'games/mariazza/alg_cpu_mariazza'
+
+require 'test/fakestuff'
 
 include Log4r
 
@@ -21,48 +24,6 @@ include Log4r
 # Test suite for testing 
 class Test_mariazza_core < Test::Unit::TestCase
   attr_reader :log
-  #
-  # Class used to intercept log to recognize errors and warning
-  class FakeIO < IO
-    attr_accessor :warn_count, :error_count
-    
-    def initialize(arg1,arg2)
-      super(arg1,arg2)
-      reset_counts
-    end
-    
-    def reset_counts
-      @warn_count = 0; @error_count = 0;
-      @points_state = []
-    end
-    
-    def print(*args)
-      #print(args)
-      str = args.slice!(0)
-      aa = str.split(':')
-      if aa[0] =~ /WARN/
-        @warn_count += 1
-      elsif aa[0] =~ /ERROR/
-        @error_count += 1
-      elsif aa[1] =~ /Punteggio attuale/
-        @points_state << aa[2].gsub(" ", "").chomp
-      end
-    end
-    
-    ##
-    # Check if points str_points was reached
-    def punteggio_raggiunto(str_points)
-      str_points.gsub!(" ", "")
-      #p @points_state
-      aa = @points_state.index(str_points)
-      if aa
-        # points state found
-        return true
-      end
-      return false
-    end
-    
-  end#end FakeIO
   
   def setup
     @log = Log4r::Logger.new("coregame_log")
@@ -85,11 +46,7 @@ class Test_mariazza_core < Test::Unit::TestCase
   def test_cpu_change7
     rep = ReplayerManager.new(@log)
     match_info = YAML::load_file(File.dirname(__FILE__) + '/saved_games/mariaz_sett_cam_brisc.yaml')
-    player = PlayerOnGame.new("Gino B.", nil, :cpu_alg, 0)
-    player2 = PlayerOnGame.new("Toro", nil, :cpu_alg, 0)
-    alg_cpu = AlgCpuMariazza.new(player, @core, nil)
-    alg_coll = { "Gino B." => alg_cpu, "Toro" => AlgCpuMariazza.new(player2, @core, nil) } 
-    rep.replay_match(@core, match_info, alg_coll, 0)
+    rep.replay_match(@core, match_info, {}, 0)
     assert_equal(0, @io_fake.warn_count)
     assert_equal(0, @io_fake.error_count)
   end
